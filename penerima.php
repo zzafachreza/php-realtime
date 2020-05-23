0bd1770507eac2c06060629be71192e5bbd158cb
Your Name <zzafachreza@gmail.com>
2020-05-23 16:52:19 +0700

    penambahan fitur alert

--- penerima.php
+++ penerima.php
@@ -21,6 +21,8 @@ session_start();
   <body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+       <script src="push.min.js"></script>
+       <script src="push.min.js.map"></script>
 </head>
 <body>
 <div class="container">
@@ -38,6 +40,9 @@ session_start();
 		<input type="text" name="user">
 		<button type="submit">enter nama</button>
 	</form>
+	<div class="alert alert-success" role="alert" style="display: none">
+  ada notifikasi baru
+</div> 
 <table class="table table-bordered">
 <span id="user"><?php echo $_SESSION['user']; ?></span>
 <thead>
@@ -53,6 +58,8 @@ session_start();
 </div>
 </body>
 
+
+
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
@@ -66,7 +73,16 @@ session_start();
 
 		if($("#user").text()==data.user){
 
+
+
 		$("tbody").append("<tr><td>"+data.pesan+"</td><td>"+data.waktu+"</td></tr>")
+
+
+		$(".alert").show().text(data.pesan);
+
+            setTimeout(function(){
+              $(".alert").hide()
+            },2000);
 
 		}
 	})
